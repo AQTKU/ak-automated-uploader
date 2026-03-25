7c0e8aeb53e3346311e88337b5b50327e0e20a86
AQTKU <aquatakat+@gmail.com>
2026-03-25 12:09:11 -0600

    Little underscore shuffle in log()

--- src/lib/server/util/log.ts
+++ src/lib/server/util/log.ts
@@ -2,7 +2,7 @@ import { Temporal } from '@js-temporal/polyfill';
 import { color } from 'bun';
 import { EOL } from 'node:os';
 
-export function log(message: string, _color: 'tomato' | 'khaki' | 'aquamarine' | 'lightgrey' = 'lightgrey') {
+export function log(message: string, color_: 'tomato' | 'khaki' | 'aquamarine' | 'lightgrey' = 'lightgrey') {
 
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     const now = Temporal.Now.instant().toZonedDateTimeISO(timeZone);
@@ -11,12 +11,12 @@ export function log(message: string, _color: 'tomato' | 'khaki' | 'aquamarine' |
     const prefix = color('grey', 'ansi-16m') + time + color('cornflowerblue', 'ansi-16m') + ' [ak-automated-uploader] ';
     const suffix = '\x1B[0m';
 
-    switch (_color) {
+    switch (color_) {
         case 'tomato': case 'khaki':
-            process.stderr.write(prefix + color(_color, 'ansi-16m') + message + suffix + EOL);
+            process.stderr.write(prefix + color(color_, 'ansi-16m') + message + suffix + EOL);
             break;
         case 'aquamarine': default:
-            process.stdout.write(prefix + color(_color, 'ansi-16m') + message + suffix + EOL);
+            process.stdout.write(prefix + color(color_, 'ansi-16m') + message + suffix + EOL);
             break;
     }
 }
