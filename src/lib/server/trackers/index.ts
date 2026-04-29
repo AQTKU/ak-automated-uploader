a6e42575db13f297e3d01ed7fc969c3f5a7a885a
Azure <100251177+AzureBelmont@users.noreply.github.com>
2026-04-29 09:33:13 -0400

    Add MNS tracker to trackers index

--- src/lib/server/trackers/index.ts
+++ src/lib/server/trackers/index.ts
@@ -2,8 +2,10 @@ import type { SettingsField, TrackerField, TrackerSettings } from '$lib/types';
 import Aither, { settings as aitherSettings, fields as aitherFields } from './aither';
 import type Tracker from '../tracker';
 import LST, { settings as lstSettings, fields as lstFields } from './lst';
+import MNS, { settings as mnsSettings, fields as mnsFields } from './mns';
 
 export const trackers: Record<string, { class: new (settings: TrackerSettings) => Tracker, settings: SettingsField[], fields: TrackerField[] }> = {
     'Aither': { class: Aither, settings: aitherSettings, fields: aitherFields },
     'LST': { class: LST, settings: lstSettings, fields: lstFields },
+    'MNS': { class: MNS, settings: mnsSettings, fields: mnsFields },
 };
