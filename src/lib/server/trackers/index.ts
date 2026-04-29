35f421421a6908c80ad9dbdfdd45a1353aaa0b97
Azure <100251177+AzureBelmont@users.noreply.github.com>
2026-04-29 14:25:32 -0400

    Add Seedpool tracker to the trackers index

--- src/lib/server/trackers/index.ts
+++ src/lib/server/trackers/index.ts
@@ -2,8 +2,10 @@ import type { SettingsField, TrackerField, TrackerSettings } from '$lib/types';
 import Aither, { settings as aitherSettings, fields as aitherFields } from './aither';
 import type Tracker from '../tracker';
 import LST, { settings as lstSettings, fields as lstFields } from './lst';
+import Seedpool, { settings as seedpoolSettings, fields as seedpoolFields } from './seedpool';
 
 export const trackers: Record<string, { class: new (settings: TrackerSettings) => Tracker, settings: SettingsField[], fields: TrackerField[] }> = {
     'Aither': { class: Aither, settings: aitherSettings, fields: aitherFields },
     'LST': { class: LST, settings: lstSettings, fields: lstFields },
+    'Seedpool': { class: Seedpool, settings: seedpoolSettings, fields: seedpoolFields },
 };
