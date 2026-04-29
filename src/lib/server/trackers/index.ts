c5731d0b3c9d59a05a06d615c6345354466f7e89
Azure <100251177+AzureBelmont@users.noreply.github.com>
2026-04-29 10:23:10 -0400

    Add SP tracker to index

--- src/lib/server/trackers/index.ts
+++ src/lib/server/trackers/index.ts
@@ -3,9 +3,11 @@ import Aither, { settings as aitherSettings, fields as aitherFields } from './ai
 import type Tracker from '../tracker';
 import LST, { settings as lstSettings, fields as lstFields } from './lst';
 import MNS, { settings as mnsSettings, fields as mnsFields } from './mns';
+import SP, { settings as spSettings, fields as spFields } from './sp';
 
 export const trackers: Record<string, { class: new (settings: TrackerSettings) => Tracker, settings: SettingsField[], fields: TrackerField[] }> = {
     'Aither': { class: Aither, settings: aitherSettings, fields: aitherFields },
     'LST': { class: LST, settings: lstSettings, fields: lstFields },
     'MNS': { class: MNS, settings: mnsSettings, fields: mnsFields },
+    SP: { class: SP, settings: spSettings, fields: spFields },
 };
