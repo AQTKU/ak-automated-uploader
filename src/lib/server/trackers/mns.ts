15eb9e4480c76f858da16744c97ab3dec333961d
Azure <100251177+AzureBelmont@users.noreply.github.com>
2026-04-29 11:13:51 -0400

    Refactor MNS tracker fields and default values

--- src/lib/server/trackers/mns.ts
+++ src/lib/server/trackers/mns.ts
@@ -6,8 +6,6 @@ import type {
     TrackerSettings,
     TrackerSearchResults,
     Metadata,
-    TrackerLayout,
-    TrackerAfterUploadAction
 } from '$lib/types';
 
 import * as v from 'valibot';
@@ -31,6 +29,7 @@ const types: KeyValueData = [
     ['4', 'WEB-DL'],
     ['5', 'WEBRip'],
     ['6', 'HDTV'],
+    ['7', 'Other'],
 ];
 
 const resolutions: KeyValueData = [
@@ -65,92 +64,222 @@ export const settings: SettingsField[] = [
 
 export const fields = [
     { key: 'name', label: 'Title', type: 'text', default: '' },
-    { key: 'categoryId', label: 'Category', type: 'select', default: 'Movies', options: categories },
-    { key: 'typeId', label: 'Type', type: 'select', default: 'Other', options: types },
-    { key: 'resolutionId', label: 'Resolution', type: 'select', default: 'Other', options: resolutions },
+
+    { key: 'categoryId', label: 'Category', type: 'select', default: '1', options: categories },
+
+    { key: 'typeId', label: 'Type', type: 'select', default: '7', options: types },
+
+    { key: 'resolutionId', label: 'Resolution', type: 'select', default: '10', options: resolutions },
+
     { key: 'provider', label: 'Provider', type: 'text', default: '' },
+
     { key: 'distributorId', label: 'Distributor', type: 'select', default: '', options: unit3dDistributors },
+
     { key: 'regionId', label: 'Region', type: 'select', default: '', options: unit3dRegions },
+
     { key: 'seasonNumber', label: 'Season', type: 'text', default: '' },
+
     { key: 'episodeNumber', label: 'Episode', type: 'text', default: '' },
+
     { key: 'tmdb', label: 'TMDB ID', type: 'text', default: '' },
+
     { key: 'imdb', label: 'IMDB ID', type: 'text', default: '' },
+
     { key: 'tvdb', label: 'TVDB ID', type: 'text', default: '' },
+
     { key: 'mal', label: 'MAL ID', type: 'text', default: '' },
+
     { key: 'keywords', label: 'Keywords', type: 'text', default: '' },
-    { key: 'description', label: 'Description', type: 'multiline', default: '' },
+
+    {
+        key: 'description',
+        label: 'Description',
+        type: 'multiline',
+        default: '{% screenshots width:350 %}[url={{page}}][img=350]{{thumbnail}}[/img][/url]{% endscreenshots %}',
+    },
+
     { key: 'mediainfo', label: 'MediaInfo', type: 'multiline', default: '' },
+
     { key: 'bdinfo', label: 'BDInfo', type: 'multiline', default: '' },
+
     { key: 'anonymous', label: 'Anonymous', type: 'checkbox', default: false },
+
     { key: 'personalRelease', label: 'Personal release', type: 'checkbox', default: false },
+
     { key: 'modQueueOptIn', label: 'Opt in to mod queue', type: 'checkbox', default: false },
+
     { key: 'draftQueueOptIn', label: 'Save as draft', type: 'checkbox', default: false },
+
     { key: 'internal', label: 'Internal', type: 'checkbox', default: false },
+
     { key: 'refundable', label: 'Refundable', type: 'checkbox', default: false },
+
 ] as const satisfies TrackerField[];
 
 export default class MNS extends Tracker {
+
     apiKey = '';
+
     override name = 'MNS';
+
     override data: FieldsToType<typeof fields>;
+
     override readonly fields = fields;
 
     constructor(settings: TrackerSettings) {
+
         super(settings);
+
         this.data = this.setDefaults(this.fields);
 
-        if (!settings.apiKey) throw Error('API key is missing for MidnightScene');
-        this.apiKey = settings.apiKey;
+        this.apiKey = settings.apiKey ?? '';
+
+        if (settings.defaultDescription) {
+            this.data.description = settings.defaultDescription;
+        }
+
     }
 
     private get headers(): Headers {
+
         return new Headers({
             'Accept': 'application/json',
             'Authorization': `Bearer ${this.apiKey}`,
         });
+
     }
 
     applyMetadata(metadata: Metadata) {
+
         this.data.tmdb = String(metadata.tmdbId);
-        this.data.imdb = metadata.imdbId?.replace(/^tt/i, '') ?? '0';
-        this.data.tvdb = metadata.tvdbId ? String(metadata.tvdbId) : '0';
-        this.data.mal = metadata.malId ? String(metadata.malId) : '0';
+
+        this.data.imdb = metadata.imdbId
+            ? metadata.imdbId.replace(/^tt/i, '')
+            : '0';
+
+        this.data.tvdb = metadata.tvdbId
+            ? String(metadata.tvdbId)
+            : '0';
+
+        this.data.mal = metadata.malId
+            ? String(metadata.malId)
+            : '0';
+
         this.data.keywords = metadata.keywords.join(', ');
+
+    }
+
+    applyRelease(release: Release) {
+
+        this.setOption('resolutionId', '10');
+
+        if (release.resolution) {
+            this.setOption('resolutionId', release.resolution);
+        }
+
+        this.setOption('typeId', '7');
+
+        if (release.fullDisc) {
+
+            this.setOption('typeId', '1');
+
+        } else if (release.remux) {
+
+            this.setOption('typeId', '2');
+
+        } else if (
+            release.source === 'BluRay' ||
+            release.source === 'DVDRip'
+        ) {
+
+            this.setOption('typeId', '3');
+
+        } else if (release.source?.endsWith('WEB-DL')) {
+
+            this.setOption('typeId', '4');
+
+        } else if (release.source?.endsWith('WEBRip')) {
+
+            this.setOption('typeId', '5');
+
+        } else if (release.source === 'HDTV') {
+
+            this.setOption('typeId', '6');
+
+        }
+
+        if (release.category === 'tv') {
+
+            this.setOption('categoryId', '2');
+
+            this.data.seasonNumber = release.season
+                ? String(release.season)
+                : '0';
+
+            this.data.episodeNumber = release.episode
+                ? String(release.episode)
+                : '0';
+
+        } else {
+
+            this.setOption('categoryId', '1');
+
+        }
+
+        if (release.streamingService) {
+            this.data.provider = release.streamingService;
+        }
+
+        this.data.name = release.format(
+            '{title aka} {year} {resolution} {source} {audio} {video} -{group}'
+        );
+
     }
 
     async search(): Promise<TrackerSearchResults> {
+
         const url = new URL(SEARCH_URL);
+
         const params = url.searchParams;
 
         params.append('tmdbId', this.data.tmdb);
+
         params.append('categories[]', this.data.categoryId);
+
         params.append('resolutions[]', this.data.resolutionId);
+
         params.append('types[]', this.data.typeId);
 
-        const response = await fetch(url, { headers: this.headers });
+        const response = await fetch(url, {
+            headers: this.headers,
+        });
+
         const body = await response.json();
 
         const validated = v.parse(
             v.object({
-                data: v.array(v.object({
-                    id: v.string(),
-                    attributes: v.object({
-                        name: v.string(),
-                        details_link: v.pipe(v.string(), v.url()),
-                    }),
-                }))
+                data: v.array(
+                    v.object({
+                        id: v.string(),
+                        attributes: v.object({
+                            name: v.string(),
+                            details_link: v.pipe(v.string(), v.url()),
+                        }),
+                    })
+                ),
             }),
             body
         ).data;
 
-        return validated.map(r => ({
-            name: r.attributes.name,
-            url: r.attributes.details_link,
+        return validated.map(result => ({
+            name: result.attributes.name,
+            url: result.attributes.details_link,
         }));
+
     }
 
     override async validate() {
         return;
     }
-}
 
+}
