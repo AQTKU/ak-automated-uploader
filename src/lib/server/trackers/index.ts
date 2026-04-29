37f019bb64d967380f0b9c5dda5fdff7436a52e1
Azure <100251177+AzureBelmont@users.noreply.github.com>
2026-04-29 15:19:27 -0400

    Add MidnightScene tracker to the trackers list

--- src/lib/server/trackers/index.ts
+++ src/lib/server/trackers/index.ts
@@ -2,10 +2,12 @@ import type { SettingsField, TrackerField, TrackerSettings } from '$lib/types';
 import Aither, { settings as aitherSettings, fields as aitherFields } from './aither';
 import type Tracker from '../tracker';
 import LST, { settings as lstSettings, fields as lstFields } from './lst';
+import MidnightScene, { settings as midnightsceneSettings, fields as midnightsceneFields } from './midnightscene';
 import Seedpool, { settings as seedpoolSettings, fields as seedpoolFields } from './seedpool';
 
 export const trackers: Record<string, { class: new (settings: TrackerSettings) => Tracker, settings: SettingsField[], fields: TrackerField[] }> = {
     'Aither': { class: Aither, settings: aitherSettings, fields: aitherFields },
     'LST': { class: LST, settings: lstSettings, fields: lstFields },
+    'MidnightScene': { class: MidnightScene, settings: midnightsceneSettings, fields: midnightsceneFields },
     'Seedpool': { class: Seedpool, settings: seedpoolSettings, fields: seedpoolFields },
 };
