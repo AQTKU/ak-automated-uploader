e6e75aee9340aab24077f050923f4d7c2e5fb710
AQTKU <aquatakat+@gmail.com>
2026-08-11 18:52:05 -0600

    Support ImgBB without API

--- src/lib/server/image-hosts/imgbb.ts
+++ src/lib/server/image-hosts/imgbb.ts
@@ -3,14 +3,18 @@ import * as v from 'valibot';
 import ImageHost from '../image-host';
 import PQueue from 'p-queue';
 import { file } from 'bun';
+import { basename } from 'node:path';
+import { Temporal } from '@js-temporal/polyfill';
 
-const UPLOAD_URL = 'https://api.imgbb.com/1/upload';
+const FRONT_PAGE_URL = 'https://imgbb.com/';
+const API_UPLOAD_URL = 'https://api.imgbb.com/1/upload';
+const PAGE_UPLOAD_URL = 'https://imgbb.com/json';
 
 export const imgBBSettings: SettingsField[] = [{
     id: 'apiKey',
     label: 'API key',
     type: 'password',
-    description: 'Find your API key at the <a href="https://api.imgbb.com/">API page</a> after creating an account.',
+    description: 'Optional. Find your API key at the <a href="https://api.imgbb.com/">API page</a> after creating an account.',
     default: '',
 }];
 
@@ -25,28 +29,35 @@ class ImgBB extends ImageHost {
         this.apiKey = settings.apiKey ?? '';
     }
 
-    async post(image: string, thumbnail: boolean, signal: AbortSignal): Promise<Image> {
+    async getAuthToken() {
 
-        const typedArray = await file(image).bytes();
-        const base64 = typedArray.toBase64();
+        const response = await fetch(FRONT_PAGE_URL);
+        const body = await response.text();
 
-        const formData = new FormData();
-        formData.append('key', this.apiKey);
-        formData.append('image', base64);
+        const matches = body.match(/PF\.obj\.config\.auth_token\s*=\s*(['"])(\w+?)\1/s)
+        if (!matches || !matches[2]) throw Error("Couldn't find auth token on ImgBB front page");
 
-        const response = await fetch(UPLOAD_URL, {
-            method: 'POST',
-            body: formData,
-            signal,
-        });
+        return matches[2];
 
-        const body = await response.json();
+    }
 
-        if (!response.ok || !body.success) {
+    async postToApi(image: string, thumbnail: boolean, signal: AbortSignal) {
+
+        const bytes = await file(image).bytes();
+        const base64 = bytes.toBase64();
+
+        const body = new FormData();
+        body.append('key', this.apiKey);
+        body.append('image', base64);
+
+        const response = await fetch(API_UPLOAD_URL, { method: 'POST', body, signal });
+        const responseBody = await response.json();
+
+        if (!response.ok || !responseBody.success) {
             const ErrorSchema = v.object({ error: v.object({ message: v.string() })});
-            const error = v.safeParse(ErrorSchema, body);
+            const error = v.safeParse(ErrorSchema, responseBody);
             if (error.success) throw Error(error.output.error.message);
-            throw Error(body.status_txt ?? response.statusText);
+            throw Error(responseBody.status_txt ?? response.statusText);
         }
 
         const Schema = v.object({
@@ -55,21 +66,62 @@ class ImgBB extends ImageHost {
                 image: v.object({ url: v.pipe(v.string(), v.url()) }),
                 thumb: v.object({ url: v.pipe(v.string(), v.url()) }),
                 medium: v.object({ url: v.pipe(v.string(), v.url()) }),
-            })
-        })
+            }),
+        });
 
-        const validated = v.parse(Schema, body);
+        const validated = v.parse(Schema, responseBody);
 
         return {
             page: validated.data.url_viewer,
             image: validated.data.image.url,
             thumbnail: thumbnail ? validated.data.thumb.url : validated.data.medium.url,
+        } satisfies Image;
+
+    }
+
+    async postToPage(image: string, thumbnail: boolean, signal: AbortSignal) {
+
+        const authToken = await this.getAuthToken();
+
+        const body = new FormData();
+        body.set('source', file(image), basename(image));
+        body.set('type', 'file');
+        body.set('action', 'upload');
+        body.set('timestamp', Temporal.Now.instant().epochMilliseconds.toString());
+        body.set('auth_token', authToken);
+
+        const response = await fetch(PAGE_UPLOAD_URL, { method: 'POST', body, signal });
+        const responseBody = await response.json();
+
+        if (!response.ok || !responseBody.success) {
+            const ErrorSchema = v.object({ error: v.object({ message: v.string() })});
+            const error = v.safeParse(ErrorSchema, responseBody);
+            if (error.success) throw Error(error.output.error.message);
+            throw Error(responseBody.status_txt ?? response.statusText);
         }
+
+        const Schema = v.object({
+            image: v.object({
+                url_viewer: v.pipe(v.string(), v.url()),
+                image: v.object({ url: v.pipe(v.string(), v.url()) }),
+                thumb: v.object({ url: v.pipe(v.string(), v.url()) }),
+                medium: v.object({ url: v.pipe(v.string(), v.url()) }),
+            }),
+        });
+
+        const validated = v.parse(Schema, responseBody);
+
+        return {
+            page: validated.image.url_viewer,
+            image: validated.image.image.url,
+            thumbnail: thumbnail ? validated.image.thumb.url : validated.image.medium.url,
+        } satisfies Image;
 
     }
 
     async upload(path: string, width: number | undefined, signal: AbortSignal) {
-        return await queue.add(() => this.post(path, !width, signal));
+        if (this.apiKey) return await queue.add(() => this.postToApi(path, !width, signal));
+        else return await queue.add(() => this.postToPage(path, !width, signal));
     }
 
 }
